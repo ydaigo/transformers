2aa80b2a7cc732ade503a506688b0208ff4d23bc
ydaigo <b1614808@planet.kanazawa-it.ac.jp>
2020-04-05 18:12:18 +0900

    a

--- examples/run_glue.py
+++ examples/run_glue.py
@@ -56,7 +56,7 @@ from transformers import (
     XLNetTokenizer,
     get_linear_schedule_with_warmup,
 )
-)
+
 from transformers import glue_compute_metrics as compute_metrics
 from transformers import glue_convert_examples_to_features as convert_examples_to_features
 from transformers import glue_output_modes as output_modes
